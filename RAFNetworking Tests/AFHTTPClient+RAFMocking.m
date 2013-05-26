1de646bdb1adaa0783e08ab7d25efd094d388181
CodaFi <devteam.codafi@gmail.com>
2013-05-26 17:54:15 -0600

    Finally got Unit Tests set up

---
+++ RAFNetworking Tests/AFHTTPClient+RAFMocking.m
@@ -0,0 +1,46 @@
+//
+//  AFHTTPClient+RAFMocking.m
+//  Reactive AFNetworking Example
+//
+//  Created by Robert Widmann on 5/26/13.
+//  Copyright (c) 2013 CodaFi. All rights reserved.
+//
+
+NSString *const AFHTTPClientMockResponse = @"AFHTTPClientMockResponse";
+
+#import "AFHTTPClient+RAFMocking.h"
+
+@implementation AFHTTPClient (RAFMocking)
+
+- (RACSignal *)mock_getPath:(NSString *)path parameters:(NSDictionary *)parameters {
+    return [self mock_requestPath:path parameters:parameters method:@"GET"];
+}
+
+- (RACSignal *)mock_postPath:(NSString *)path parameters:(NSDictionary *)parameters {
+    return [self mock_requestPath:path parameters:parameters method:@"POST"];
+}
+
+- (RACSignal *)mock_putPath:(NSString *)path parameters:(NSDictionary *)parameters {
+    return [self mock_requestPath:path parameters:parameters method:@"PUT"];
+}
+
+- (RACSignal *)mock_deletePath:(NSString *)path parameters:(NSDictionary *)parameters {
+    return [self mock_requestPath:path parameters:parameters method:@"DELETE"];
+}
+
+- (RACSignal *)mock_patchPath:(NSString *)path parameters:(NSDictionary *)parameters {
+    return [self mock_requestPath:path parameters:parameters method:@"PATCH"];
+}
+
+- (RACSignal *)mock_requestPath:(NSString *)path parameters:(NSDictionary *)parameters method:(NSString *)method {
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+		
+		[subscriber sendNext:AFHTTPClientMockResponse];
+		[subscriber sendCompleted];
+		
+        return nil;
+    }];
+}
+
+
+@end
--- RAFNetworking Tests/RAFHTTPClientSpec.m
+++ RAFNetworking Tests/RAFHTTPClientSpec.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 CodaFi. All rights reserved.
 //
 
+#import "AFHTTPClient+RAFMocking.h"
+
 SpecBegin(RAFHTTPClient)
 
 __block AFHTTPClient *client = nil;
@@ -16,9 +18,117 @@ beforeEach(^{
 });
 
 describe(@"-rac_getPath:parameters:", ^{
+	it(@"should create an immutable signal", ^{
+		RACSignal *getSignal = [client mock_getPath:@"http://www.example.com" parameters:nil];
+		expect(getSignal).notTo.beNil();
+		expect(^{
+			[getSignal performSelector:@selector(sendNext:) withObject:NSNull.null];
+		}).to.raise(NSInvalidArgumentException);
+		expect(^{
+			[getSignal performSelector:@selector(sendError:) withObject:NSNull.null];
+		}).to.raise(NSInvalidArgumentException);
+		expect(^{
+			[getSignal performSelector:@selector(sendCompleted)];
+		}).to.raise(NSInvalidArgumentException);
+	});
 	
-	it(@"should create", ^{
-client rac_getPath:<#(NSString *)#> parameters:<#(NSDictionary *)#>
+	it(@"should GET a path", ^{
+		[[client mock_getPath:@"http://www.example.com" parameters:nil]subscribeNext:^(id x) {
+			expect(x).notTo.beNil();
+			expect(x).to.equal(AFHTTPClientMockResponse);
+		}];
+	});
+});
+
+describe(@"-rac_putPath:parameters:", ^{
+	it(@"should create an immutable signal", ^{
+		RACSignal *putSignal = [client rac_postPath:@"http://www.example.com" parameters:nil];
+		expect(putSignal).notTo.beNil();
+		expect(^{
+			[putSignal performSelector:@selector(sendNext:) withObject:NSNull.null];
+		}).to.raise(NSInvalidArgumentException);
+		expect(^{
+			[putSignal performSelector:@selector(sendError:) withObject:NSNull.null];
+		}).to.raise(NSInvalidArgumentException);
+		expect(^{
+			[putSignal performSelector:@selector(sendCompleted)];
+		}).to.raise(NSInvalidArgumentException);
+	});
+	
+	it(@"should GET a path", ^{
+		[[client mock_putPath:@"http://www.example.com" parameters:nil]subscribeNext:^(id x) {
+			expect(x).notTo.beNil();
+			expect(x).to.equal(AFHTTPClientMockResponse);
+		}];
+	});
+});
+
+describe(@"-rac_postPath:parameters:", ^{
+	it(@"should create an immutable signal", ^{
+		RACSignal *postSignal = [client rac_postPath:@"http://www.example.com" parameters:nil];
+		expect(postSignal).notTo.beNil();
+		expect(^{
+			[postSignal performSelector:@selector(sendNext:) withObject:NSNull.null];
+		}).to.raise(NSInvalidArgumentException);
+		expect(^{
+			[postSignal performSelector:@selector(sendError:) withObject:NSNull.null];
+		}).to.raise(NSInvalidArgumentException);
+		expect(^{
+			[postSignal performSelector:@selector(sendCompleted)];
+		}).to.raise(NSInvalidArgumentException);
+	});
+	
+	it(@"should GET a path", ^{
+		[[client mock_postPath:@"http://www.example.com" parameters:nil]subscribeNext:^(id x) {
+			expect(x).notTo.beNil();
+			expect(x).to.equal(AFHTTPClientMockResponse);
+		}];
+	});
+});
+
+describe(@"-rac_deletePath:parameters:", ^{
+	it(@"should create an immutable signal", ^{
+		RACSignal *deleteSignal = [client rac_deletePath:@"http://www.example.com" parameters:nil];
+		expect(deleteSignal).notTo.beNil();
+		expect(^{
+			[deleteSignal performSelector:@selector(sendNext:) withObject:NSNull.null];
+		}).to.raise(NSInvalidArgumentException);
+		expect(^{
+			[deleteSignal performSelector:@selector(sendError:) withObject:NSNull.null];
+		}).to.raise(NSInvalidArgumentException);
+		expect(^{
+			[deleteSignal performSelector:@selector(sendCompleted)];
+		}).to.raise(NSInvalidArgumentException);
+	});
+	
+	it(@"should GET a path", ^{
+		[[client mock_deletePath:@"http://www.example.com" parameters:nil]subscribeNext:^(id x) {
+			expect(x).notTo.beNil();
+			expect(x).to.equal(AFHTTPClientMockResponse);
+		}];
+	});
+});
+
+describe(@"-rac_patchPath:parameters:", ^{
+	it(@"should create an immutable signal", ^{
+		RACSignal *patchSignal = [client rac_patchPath:@"http://www.example.com" parameters:nil];
+		expect(patchSignal).notTo.beNil();
+		expect(^{
+			[patchSignal performSelector:@selector(sendNext:) withObject:NSNull.null];
+		}).to.raise(NSInvalidArgumentException);
+		expect(^{
+			[patchSignal performSelector:@selector(sendError:) withObject:NSNull.null];
+		}).to.raise(NSInvalidArgumentException);
+		expect(^{
+			[patchSignal performSelector:@selector(sendCompleted)];
+		}).to.raise(NSInvalidArgumentException);
+	});
+	
+	it(@"should GET a path", ^{
+		[[client mock_patchPath:@"http://www.example.com" parameters:nil]subscribeNext:^(id x) {
+			expect(x).notTo.beNil();
+			expect(x).to.equal(AFHTTPClientMockResponse);
+		}];
 	});
 });
 
