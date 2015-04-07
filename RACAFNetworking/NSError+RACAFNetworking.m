bce87f7b47d8c4b3e62f7a416132f3ca42f93217
Dal Rupnik <legoless@gmail.com>
2015-04-07 13:38:42 +0200

    Removed license

--- RACAFNetworking/NSError+RACAFNetworking.m
+++ RACAFNetworking/NSError+RACAFNetworking.m
@@ -1,31 +1,12 @@
 //
 // NSError+RACAFNetworking.m
 //
-// The MIT License (MIT)
-//
 // Copyright (c) 2015 Dal Rupnik (https://github.com/legoless)
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
 //
 
 #import "NSError+RACAFNetworking.h"
 #import "AFURLRequestSerialization.h"
+#import "AFURLResponseSerialization.h"
 
 @implementation NSError (RACAFNetworking)
 
@@ -67,7 +48,7 @@
         return urlResponse.URL.absoluteString;
     }
     else {
-        return (self.originalError.userInfo[NSURLErrorFailingURLStringErrorKey]) ? self.originalError.userInfo[NSURLErrorFailingURLStringErrorKey] : nil;
+        return (self.userInfo[NSURLErrorFailingURLStringErrorKey]) ? self.userInfo[NSURLErrorFailingURLStringErrorKey] : nil;
     }
 }
 
